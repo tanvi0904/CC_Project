1a28bdd6786b6109703e2c588cbeb9794f1cf789
UtkarshBagaria <79400700+UtkarshBagaria@users.noreply.github.com>
2023-04-18 22:59:34 +0530

    removed corsing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -52,9 +52,7 @@ function App() {
 
   const addTodo = text => {
     console.log(text);
-    Axios.post('http://54.189.145.65:81/api/insert',{listname:text}, {
-      withCredentials: true
-    });
+    Axios.post('http://54.189.145.65:81/api/insert',{listname:text});
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
   };
@@ -62,9 +60,7 @@ function App() {
   const markTodo = index => {
     // console.log(index);
     const newTodos = [...todos];
-    Axios.post('http://54.189.145.65:81/api/update',{listname:newTodos[index].text}, {
-      withCredentials: true
-    });
+    Axios.post('http://54.189.145.65:81/api/update',{listname:newTodos[index].text});
     console.log(newTodos[index].text);
     newTodos[index].isDone = true;
     setTodos(newTodos);
@@ -72,9 +68,7 @@ function App() {
 
   const removeTodo = index => {
     const newTodos = [...todos];
-    Axios.post('http://54.189.145.65:81/api/remove',{listname:newTodos[index].text}, {
-      withCredentials: true
-    });
+    Axios.post('http://54.189.145.65:81/api/remove',{listname:newTodos[index].text});
     newTodos.splice(index, 1);
     setTodos(newTodos);
   };
