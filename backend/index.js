bb8ed2e1c037a2a13f00f0bba95dc99b346e8156
tanvi0904 <79040060+tanvi0904@users.noreply.github.com>
2023-04-20 17:02:25 +0530

    new trial

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,41 @@
 const express = require('express');
-const cors=require('cors');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const db=mysql.createPool({
-    host: 'ccdatabase.c95irdynjpct.us-west-2.rds.amazonaws.com',
-    user: 'admin',
-    password: 'database',
-    database: 'CCdatabase'
-})
+const db = mysql.createPool({
+  host: 'ccdatabase.c95irdynjpct.us-west-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'database',
+  database: 'CCdatabase'
+});
 
-const corsOptions = {
-  origin: '*',
-  methods: '*',
-  allowedHeaders: '*'
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
-app.post("/api/insert",(req,res)=>{
-    const sqlinsert="INSERT INTO todolist (listname,completion) VALUES (?,0)";
-    db.query(sqlinsert,[req.body.listname],(err,result)=>{
-        console.log(req.body.listname);
-    });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
-app.post("/api/update",(req,res)=>{
-    const sqlupd="UPDATE todolist SET completion=1 WHERE listname=?";
-    db.query(sqlupd,[req.body.listname],(err,result)=>{
-        console.log(req.body.listname);
-    });
+
+app.post('/api/insert', (req, res) => {
+  const sqlinsert = 'INSERT INTO todolist (listname,completion) VALUES (?,0)';
+  db.query(sqlinsert, [req.body.listname], (err, result) => {
+    console.log(req.body.listname);
+  });
 });
-app.post("/api/remove",(req,res)=>{
-    const sqlupd="DELETE from todolist WHERE listname=?";
-    db.query(sqlupd,[req.body.listname],(err,result)=>{
-        console.log(req.body.listname);
-    });
+
+app.post('/api/update', (req, res) => {
+  const sqlupd = 'UPDATE todolist SET completion=1 WHERE listname=?';
+  db.query(sqlupd, [req.body.listname], (err, result) => {
+    console.log(req.body.listname);
+  });
+});
+
+app.post('/api/remove', (req, res) => {
+  const sqlupd = 'DELETE from todolist WHERE listname=?';
+  db.query(sqlupd, [req.body.listname], (err, result) => {
+    console.log(req.body.listname);
+  });
 });
 
 app.listen(3001, () => {
